b82458ccdc4f4fd228e8d8f8f8188d1ac2e13902
Emma <em.smyth5@gmail.com>
2023-12-12 20:48:00 +0000

    update document titles

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -62,6 +62,18 @@ export default function MovieDetails({
     [selectedId, KEY]
   );
 
+  useEffect(
+    function () {
+      if (!title) return;
+      document.title = `Movie | ${title}`;
+
+      return function () {
+        document.title = "usePopcorn";
+      };
+    },
+    [title]
+  );
+
   return (
     <div className="details">
       {isLoading ? (
